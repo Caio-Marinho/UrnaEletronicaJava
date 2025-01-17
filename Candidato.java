6ed019036877d924d12fbbbe72acfdbc179cc8e9
Caio-Marinho <kaigabriel12@gmail.com>
2025-01-17 17:41:09 -0300

    Metodo mostrar candidatos

--- Candidato.java
+++ Candidato.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+
 public class Candidato {
     // Atributos para representar um candidato
     private String _nome;
     private int _numero;
     private int _votos;
+
+        public Candidato(){
+            
+        }
     
         // Construtor para criar um novo candidato
         public Candidato (int numero,String candidato) {
@@ -10,7 +16,7 @@ public class Candidato {
             this._numero = numero;
             this._votos = 0;
         }
-    
+        
         // Métodos para obter informações sobre o candidato
         public String getNome() {
             return this._nome;
@@ -27,5 +33,12 @@ public class Candidato {
     // Método para incrementar o número de votos
     public void incrementarVotos() {
         this._votos++;
+    }
+
+    public void mostrarCandidatos(ArrayList<Candidato> candidato){
+        
+        for (Candidato candidatos : candidato) {
+            System.out.println("0"+candidatos.getNumero() + " - " + candidatos.getNome());
+        }
     }
 }
